0668d12288e0dc71be0ff59b2c5ac36ea550e22c
it21280160 Theekshana <99546709+IT21280160@users.noreply.github.com>
2022-03-22 04:31:28 +0000

    Exercise 01 , 02 done

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,18 @@
 #include "Student.h"
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int st_ID, char st_name[]) {
   
+  studentId = st_ID;
+  strcpy(name, st_name);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() {
+
+  cout << "Student ID : " << studentId << endl;
+  cout << "Student Name : " << name << endl;
 }
